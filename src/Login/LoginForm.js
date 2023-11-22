bfc5d11c259080ff4a81b77a885537aa078d080b
lagataricamarie <ricamarielagata@thelewiscollege.edu.ph>
2023-11-23 00:01:16 +0800

    Login Form

---
+++ src/Login/LoginForm.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import { loginStyle } from '../LoginStyleCom/loginStlyle';
+
+const LoginForm = () => {
+  return (
+    <div style={Container}>
+      <form style={formContainer}>
+        <h2 style={{ textAlign: "center", color: "#000" }}>
+            Login
+        </h2>
+        <div style={{ display: "flex", flexDirection: "column", width: "100%"}} >
+            <input 
+            type="email"
+            name='email'
+            placeholder='Email'
+            style={{ padding: "10px", margin: "7px 0px", border: "1px solid #cacaca", outline: "none", borderRadius: "5px" }}
+            required
+            />
+            <input 
+            type="password"
+            name='password'
+            placeholder='Password'
+            style={{ padding: "10px", margin: "7px 0px", border: "1px solid #cacaca", outline: "none",  borderRadius: "5px"}}
+            required
+            />
+
+            <div style={LinkCon}>
+            <a href='#' style={{ textDecoration: "none", textAlign: "center" }} > Forgot password? </a>
+            </div>
+
+            <button style={loginStyle.ButtonStyle} > Login </button>
+
+
+            <div style={LinkCon}>
+            <span> Don't have an account? <a style={{textDecoration: "none"}} href='#'> SignUp </a></span>
+            </div>
+
+            <div style={loginStyle.LineStyle}>
+                <span style={loginStyle.Or}> Or </span>
+            </div>
+
+            <div>
+                <a to="#" style={loginStyle.facebookStyle}>
+                <img src="/images/facebook.png" alt="" style={ImgStyle} />
+                <span> Login with Facebook </span>
+                </a>
+            </div>
+            <div>
+                <a to="#" style={loginStyle.googleStyle}>
+                <img src="/images/google.png" alt="" style={ImgStyle} />
+                <span> Login with Google </span>
+                </a>
+            </div>
+
+        </div>
+      </form>
+    </div>
+);
+};
+
+const Container = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    width: "100%",
+    backgroundColor: "#4070f4",
+    
+}
+const formContainer = {
+    backgroundColor: "white",
+    maxWidth: "300px",
+    padding: "20px",
+    width: "100%",
+    borderRadius: "10px",
+   
+}
+const LinkCon = {
+    textAlign: "center",
+    fontSize: " 0.870em"
+}
+const ImgStyle = {
+    position: "absolute",
+    display: "flex",
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "20px",
+    width: "20px",
+    borderRadius: "50%",
+    objectFit: "cover",
+    top: "50%",
+    left: "15px",
+    transform: "translateY(-50%)"
+}
+
+
+export default LoginForm;
---
+++ src/LoginStyleCom/loginStlyle.js
@@ -0,0 +1,69 @@
+const ButtonStyle = {
+    backgroundColor: "#0171d3",
+    color: "white",
+    padding: "10px",
+    border: "none",
+    borderRadius: "5px",
+    margin: "15px 0px 10px 0px ",
+    transition: "0.8s",
+    cursor: "pointer"
+}
+
+const LineStyle = {
+    position: "relative",
+    height: "1px",
+    width: "100%",
+    margin: "30px 0px",
+    backgroundColor: "#d4d4d4",
+}
+
+const Or = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "white",
+    color: "rgb(119, 116, 116)",
+    padding: "0 15px"
+}
+
+const facebookStyle = {
+    position: "relative",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#4267b2",
+    color: "white",
+    height: "39px",
+    width: "100%",
+    margin: "7px 0px",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "0.870em"
+}
+
+const googleStyle = {
+    backgroundColor: "white",
+    color: "rgb(136, 133, 133)",
+    border: "1px solid #cacaca",
+    position: "relative",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "39px",
+    width: "100%",
+    marginTop: "10px",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "0.870em"
+}
+
+
+export const loginStyle = {
+    ButtonStyle : ButtonStyle,
+    LineStyle : LineStyle,
+    Or : Or,
+    facebookStyle : facebookStyle,
+    googleStyle : googleStyle
+    
+  }
